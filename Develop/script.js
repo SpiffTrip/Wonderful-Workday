ec14f64355f3dd4c70024b4c74b7537d3d3c1534
SpiffTrip <95441222+SpiffTrip@users.noreply.github.com>
2022-01-30 19:24:57 -0700

    refactored code

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,12 @@
+const m = moment();
+//console.log(m.toString());
+//console.log(m.format("dddd MMM Mo YYYY"));
+//console.log(m.format("[the day is] dddd [and we are in] YYYY"));
+console.log(m.format("L"));
+
+var displayDate = function(){
+    $("#currentDay").append(m.format("L"));
+}
 
 
 
@@ -9,6 +18,15 @@
 
 
 
+//hour9 
+//hour10
+//hour11
+//hour12
+//hour1
+//hour2 
+//hour3 
+//hour4 
+//hour5
 
 var saveTasks = function(event) {
     console.log(event.target)
@@ -22,3 +40,9 @@ var loadTasks = function() {
     var hour9 = localStorage.getItem("hour9");
     $(".hour9").val(hour9)
 }
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  
+    // if nothing in localStorage, create a new object to track all task status arrays
+}
+displayDate();
